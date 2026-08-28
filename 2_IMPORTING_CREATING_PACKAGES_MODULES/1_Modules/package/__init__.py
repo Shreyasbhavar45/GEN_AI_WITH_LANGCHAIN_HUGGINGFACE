46cf6eea9ef57037426b2c720005908a9cca807c
Shreyas Rajaram Bhavar <shreyasbhavar0@gmail.com>
2026-08-29 00:36:54 +0530

    learning new thing on day 2

---
+++ 2_IMPORTING_CREATING_PACKAGES_MODULES/1_Modules/package/__init__.py
@@ -0,0 +1,2 @@
+"""his is a special file used in python to define packages and intitialize thier name spaces
+and its need to be in smae folder"""
---
+++ 2_IMPORTING_CREATING_PACKAGES_MODULES/1_Modules/package/test.py
@@ -0,0 +1,3 @@
+from package.math import * 
+from package.subpackages  import multiply
+print(multiply(4,5))
